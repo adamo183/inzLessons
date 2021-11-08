9fccb04b774665262f3bb9216813b15609e82be4
adamo183 <32673446+adamo183@users.noreply.github.com>
2021-11-08 23:43:10 +0100

    reservation page

---
+++ Shared/Reservation/Appointment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inzLessons.Shared.Reservation
+{
+    public class Appointment
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Description { get; set; }
+    }
+}
